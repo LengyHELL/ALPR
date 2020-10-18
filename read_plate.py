687fb01ea23edd2e691e20179d8100d821e9be72
LengyHELL <ubi19998@gmail.com>
2020-10-18 21:11:56 +0200

    Tweaking code, testing techniques

--- read_plate.py
+++ read_plate.py
@@ -4,85 +4,165 @@ import sys
 import numpy as np
 import cv2
 
+if (len(sys.argv) <= 1):
+    sys.exit(1)
+
 input_name, input_ext = sys.argv[1].split(".")
 
-blur_buffer_size = int(sys.argv[2])
+def is_inside(inside, outside):
+    in_point = 0;
+    for i in inside:
+        is_in = cv2.pointPolygonTest(outside, tuple(i[0]), False)
+        if is_in > 0:
+            return True
+    return False
 
-I = cv2.imread(input_name + "." + input_ext)
 
-if (blur_buffer_size >= 3):
-    G = cv2.GaussianBlur(I, (blur_buffer_size, blur_buffer_size), 0)
-else:
-    G = I
+img = cv2.imread(input_name + "." + input_ext)
 
-G = cv2.cvtColor(G, cv2.COLOR_BGR2GRAY)
+if img is None:
+    sys.exit(1)
 
-thr, bin = cv2.threshold(G, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+h, w, c = img.shape
 
-O = cv2.Canny(G, thr, 0.5 * thr)
+max_width = 1920
+max_height = 1080
+if (w > max_width) or (h > max_height):
+    ratio = min(max_width / w, max_height / h)
+    new_size = (round(w * ratio), round(h * ratio))
+    print("Resizing image, new size: %dx%d, %.2f%%"%(new_size[0], new_size[1], ratio))
+    img = cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
 
-cnt, hier = cv2.findContours(O, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+img_blur = img.copy()
 
-ch = zip(cnt, hier[0])
+blur_limit = float(100)
+if len(sys.argv) >= 3:
+    blur_limit = float(sys.argv[2])
 
-ch = sorted(ch, key=lambda x : cv2.contourArea(x[0]), reverse=True)
+ok = False
+while ok == False:
+    img_blur = cv2.GaussianBlur(img_blur, (3, 3), 0)
+    detected_blur = cv2.Laplacian(img_blur, cv2.CV_64F).var() * 100000 / (img.shape[0] * img.shape[1])
+    print("Blur value: %.2f"%(detected_blur))
+    if detected_blur <= blur_limit:
+        ok = True
 
-ch = [c for c in ch if (c[1][0] == -1) and (c[1][1] == -1)][:15]
+img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
 
-S = I.copy()
+thr = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[0]
+
+img_edges = cv2.Canny(img_gray, thr, 0.5 * thr)
+
+cnt, hier = cv2.findContours(img_edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+hier = hier[0]
+
+ch = [[cnt[i], hier[i], i] for i in range(len(cnt))]
+ch_top = sorted(ch, key=lambda x : cv2.contourArea(x[0]), reverse=True)[:20]
+
+possible = []
+for i in range(len(ch_top)):
+    inner = 0
+    for c in ch:
+        if c[1][3] == ch_top[i][2]:
+            inner += 1
+        if inner >= 6:
+            possible.append(i)
+            break
+
+ch = [ch_top[p] for p in possible]
+
+good = []
+for i in range(len(ch)):
+    ok = True
+    for j in range(len(ch)):
+        if (i != j) and is_inside(ch[j][0], ch[i][0]):
+            ok = False
+            break
+    if ok:
+        good.append(ch[i])
+
+ch = good
+
+img_filtered = img.copy()
 
 plates = []
-for c, h in ch:
-    epsilon = 0.02 * cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, epsilon, True)
-    if (len(approx) == 4):
-        plates.append(c)
-        #print(h)
-    else:
-        S = cv2.drawContours(S, [c], -1, (0, 0, 255), 1)
+for c, h, idx in ch:
+    found = False
+    arc_length = 0.01 * cv2.arcLength(c, True)
+    c = cv2.convexHull(c, False)
 
-m = np.zeros(G.shape, np.uint8)
-masked = cv2.drawContours(m, plates, 0, 255, -1,)
-masked = cv2.bitwise_and(O, O, mask=m)
+    for i in range(0, 21):
+        epsilon = i * arc_length
+        approx = cv2.approxPolyDP(c, epsilon, True)
+        approx = [a[0] for a in approx]
 
-D = I.copy()
+        if len(approx) == 4:
+            sides = []
+            for a in range(len(approx)):
+                b = a + 1
+                if b >= len(approx):
+                    b = 0
+                t1 = approx[a][0] - approx[b][0]
+                t2 = approx[a][1] - approx[b][1]
+                sides.append((t1 * t1 + t2 * t2)**0.5)
+
+            side_a = (sides[0] + sides[2]) / 2
+            side_b = (sides[1] + sides[3]) / 2
+
+            desired = 520 / 110;
+            current = max(side_a, side_b) / min(side_a, side_b)
+            margin = 0.3
+
+            if desired * (1 - margin) <= current <= desired * (1 + margin):
+                plates.append(c)
+                found = True
+                break
+
+    if not found:
+        img_filtered = cv2.drawContours(img_filtered, [c], -1, (0, 0, 255), 1)
+
+img_detected = img.copy()
 
 index = 0
 for p in plates:
+    mask = np.zeros(img_gray.shape, np.uint8)
+    img_masked = cv2.drawContours(mask, [p], 0, 255, -1,)
+    img_masked = cv2.bitwise_and(img_edges, img_edges, mask=mask)
+
     x, y, w, h = cv2.boundingRect(p)
+    crop_masked = img_masked[y:y+h, x:x+w]
+    crop_detected = img_detected[y:y+h, x:x+w]
 
-    crop = masked[y:y+h, x:x+w]
-    real = D[y:y+h, x:x+w]
+    cnt, hier = cv2.findContours(crop_masked, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
 
-    cnt2, hier2 = cv2.findContours(crop, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    if hier2 is None:
+    if hier is None:
+        img_filtered = cv2.drawContours(img_filtered, [p], -1, (255, 0, 255), 1.5)
         continue
 
-    ch2 = zip(cnt2, hier2[0])
-    ch2 = sorted(ch2, key=lambda x : cv2.contourArea(x[0]), reverse=True)
-    ch2 = [c for c in ch2 if c[1][1] != -1]
+    hier = hier[0]
+    ch = [[cnt[i], hier[i]] for i in range(len(cnt)) if ((hier[i][0] != -1) or (hier[i][1] != -1)) and (hier[i][3] != -1)]
+    ch = sorted(ch, key=lambda x : cv2.contourArea(x[0]), reverse=True)[:6]
 
-    #for c in ch2:
-    #    print(c[1])
+    if len(ch) >= 6:
+        img_detected = cv2.drawContours(img_detected, [p], -1, (0, 255, 0), 2)
+        cnt = [c[0] for c in ch]
+        #crop_detected = cv2.drawContours(crop_detected, cnt, -1, (255, 0, 0), 1)
+        for c in cnt:
+            x, y, w, h = cv2.boundingRect(c)
+            crop_detected = cv2.rectangle(crop_detected, (x,y), (x+w,y+h), (255, 0, 0), 1)
+    else:
+        img_filtered = cv2.drawContours(img_filtered, [p], -1, (0, 255, 255), 1)
 
-    cnt2 = [c[0] for c in ch2][:6]
-
-    #print(len(cnt2))
-
-    if len(cnt2) >= 6:
-        D = cv2.drawContours(D, [p], -1, (0, 255, 0), 2)
-        real = cv2.drawContours(real, cnt2, -1, (255, 0, 0), 1)
-
-    #cv2.imwrite(input_name + "_plate" + str(index) + "." + input_ext, real)
     index += 1
 
-#cv2.imwrite(input_name + "_processed." + input_ext, O)
-#cv2.imwrite(input_name + "_detected." + input_ext, D)
+print(index, "plates found.")
 
-cv2.imshow("Processed image", O.astype(np.uint8))
-cv2.imshow("Skipped candidates", S.astype(np.uint8))
-cv2.imshow("Detected plates", D.astype(np.uint8))
+cv2.imshow("Processed image", img_edges.astype(np.uint8))
+cv2.imshow("Filtered candidates", img_filtered.astype(np.uint8))
+
+if (len(plates) > 0):
+    cv2.imshow("Detected plates", img_detected.astype(np.uint8))
+    cv2.imshow("First detected plate", crop_masked.astype(np.uint8))
 
 cv2.waitKey()
 cv2.destroyAllWindows()
